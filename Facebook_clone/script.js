8866b12e2840a9d178a73f4c27f03c8f832d74ec
Codelord01 <davidawuniababio@gmail.com>
2023-09-24 22:37:17 +0000

    Homepage creation done

--- Facebook_clone/script.js
+++ Facebook_clone/script.js
@@ -1,11 +1,18 @@
 const settingsMenu = document.querySelector(".settings-menu");
 const profilePicture = document.querySelector(".nav-user-icon");
 const darkMode = document.querySelector("#dark-btn");
-
+const localStorageName = 'theme';
+const localStorageOptions = ['light', 'dark']
 
 function darkModeToggle(){
     darkMode.classList.toggle('dark-btn-on')
     document.body.classList.toggle('dark-theme');
+
+    if(localStorage.getItem(localStorageName) == localStorageOptions[0]){
+        localStorage.setItem(localStorageName, localStorageOptions[1])
+    }else{
+        localStorage.setItem(localStorageName, localStorageOptions[0])
+    }
 }
 
 function settingsMenuToggle(){
@@ -16,3 +23,13 @@ function settingsMenuToggle(){
 
 profilePicture.addEventListener('click', settingsMenuToggle);
 darkMode.addEventListener('click', darkModeToggle);
+
+if(localStorage.getItem(localStorageName) == localStorageOptions[0]){
+    darkMode.classList.remove('dark-btn-on');
+    document.body.classList.remove('dark-theme');
+}else if(localStorage.getItem(localStorageName) == localStorageOptions[1]){
+    darkMode.classList.add('dark-btn-on');
+    document.body.classList.add('dark-theme');
+}else{
+    localStorage.setItem(localStorageName, localStorageOptions[0]);
+}
