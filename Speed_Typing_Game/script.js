836604c6684f73c70fac4cf85078e1d4504e6f69
Codelord01 <davidawuniababio@gmail.com>
2023-09-28 21:07:26 +0000

    typing character check completed

--- Speed_Typing_Game/script.js
+++ Speed_Typing_Game/script.js
@@ -1,10 +1,31 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
+const correct = true;
 
 quoteInputElement.addEventListener('input',()=>{
-    
-} )
+   const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+   const arrayValue = quoteInputElement.value.split('');
+   arrayQuote.forEach((characterSpan, index)=>{
+    const character = arrayValue[index];
+    if(character == null){
+        characterSpan.classList.remove('correct');
+        characterSpan.classList.remove('incorrect');
+        correct = false;
+    }else if(character === characterSpan.innerText){
+        characterSpan.classList.add('correct');
+        characterSpan.classList.remove('incorrect');
+    }else{
+        characterSpan.classList.add('incorrect');
+        characterSpan.classList.remove('correct')
+        correct = false;
+    }
+   })
+
+   if(correct) renderNewQuote();
+})
+
+
 
 function getRandomQuote(){
    return fetch(RANDOM_QUOTE_API_URL)
@@ -12,9 +33,10 @@ function getRandomQuote(){
     .then(data => data.content);
 }
 
+
 async function renderNewQuote(){
     const quote = await getRandomQuote();
-    quoteDisplayElement.innerText = quote;
+    quoteDisplayElement.innerText ='';
     quote.split('').forEach(character => {
         const characterSpan = document.createElement('span');
         characterSpan.innerText = character;
@@ -22,5 +44,7 @@ async function renderNewQuote(){
     });
     quoteInputElement.value = null;
 }
+
+
 
 renderNewQuote();
