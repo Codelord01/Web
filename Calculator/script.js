2e73d79a621bcac2b6605f3024a064071ab3ff3f
Codelord01 <davidawuniababio@gmail.com>
2023-10-02 21:13:48 +0000

    functionality for deleted button added

--- Calculator/script.js
+++ Calculator/script.js
@@ -13,7 +13,7 @@ class Calculator {
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
     }
 
     appendNumber(number){
@@ -99,5 +99,11 @@ equalsButton.addEventListener('click', button =>{
 
 allClearButton.addEventListener('click', button =>{
     calculator.clear();
+    calculator.updateDisplay();
+})
+
+
+deleteButton.addEventListener('click', button =>{
+    calculator.delete();
     calculator.updateDisplay();
 })
